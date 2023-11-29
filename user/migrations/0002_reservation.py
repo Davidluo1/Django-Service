4977a47e9fa031b84632b7386b3ed2d1d0b18dd5
David Luo <LuoDavidYi@gmail.com>
2023-11-29 09:30:15 -0500

    Update 0002_reservation.py

--- user/migrations/0002_reservation.py
+++ user/migrations/0002_reservation.py
@@ -4,9 +4,7 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
-
 class Migration(migrations.Migration):
-
     dependencies = [
         ('user', '0001_initial'),
     ]

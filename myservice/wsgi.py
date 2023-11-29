cfa35759c0f60395116d183acac640ff98c5e66c
David Luo <LuoDavidYi@gmail.com>
2023-11-29 09:28:32 -0500

    Update wsgi.py

--- myservice/wsgi.py
+++ myservice/wsgi.py
@@ -1,10 +1,12 @@
 """
+
 WSGI config for myservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+
 """
 
 import os

9cd06e36aa000a760ce3eb97057f946f019326b7
David Luo <LuoDavidYi@gmail.com>
2025-03-25 09:16:34 -0400

    Update baby_sitter_reservation_view.py

--- user/views/baby_sitter_reservation_view.py
+++ user/views/baby_sitter_reservation_view.py
@@ -25,8 +25,8 @@ class BabySitterReservation(APIView):
         elif babysitter_instance.morning_shift == True:
             return Response({"msg":"Baby sitter is not available this morning"}, status=400)
             
-        # make reservation if babysitter is available in afternoon
-        if reservation_request in ("afternoon","Afternoon") and babysitter_instance.afternoon_shift == False:
+        # make reservation if babysitter is available in the afternoon
+        if reservation_request in ("afternoon", "Afternoon") and babysitter_instance.afternoon_shift == False:
             Reservation.objects.create(user=user, service_name="Baby sitter", shift=reservation_request.lower())
             babysitter_qs.update(afternoon_shift=True)
             return Response({"msg":"Reservation successful!!!"}, status=200)

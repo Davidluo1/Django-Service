468838b76c13ab63cbf15ab2f9801cf8ac219164
David Luo <LuoDavidYi@gmail.com>
2025-03-31 20:52:21 -0400

    Update nearby_pet_trainer_view.py

--- user/views/nearby_pet_trainer_view.py
+++ user/views/nearby_pet_trainer_view.py
@@ -16,11 +16,11 @@ class NearbyPetTrainer(APIView):
         user_qs = User.objects.filter(id=user_id)[0]
         user_latitude = user_qs.latitude
         user_longitude = user_qs.longitude
-        # get all pet trainers from database
+        # Get all pet trainers from database
         pet_trainer_qs = PetTrainer.objects.all()
         if pet_trainer_qs:
             pet_trainer_list, display = ServiceDistanceListHelper(user_latitude,user_longitude, pet_trainer_qs)    
-            # distance unit check
+            # Distance unit check
             if display == "meter":
                 return Response({"Service" : "Pet trainer", "Measure": "meter", "Trainer" : pet_trainer_list}, status=200)
             return Response({"Measure": "km", "msg" : pet_trainer_list}, status=200)

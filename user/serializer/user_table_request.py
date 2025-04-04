5685ae1a2d6781ed1468df5ec592c353ff8041e3
David Luo <LuoDavidYi@gmail.com>
2025-04-04 11:07:21 -0400

    Update user_table_request.py

--- user/serializer/user_table_request.py
+++ user/serializer/user_table_request.py
@@ -7,9 +7,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         # pop off the password in database
         password = validated_data.pop("password")
-        # create a user class to store the rest data
+        # create a user class to store the rest of the data
         instance = self.Meta.model(**validated_data)
-        # encrypt password
+        # Encrypt password
         instance.set_password(password)
         # Save data
         instance.save()
